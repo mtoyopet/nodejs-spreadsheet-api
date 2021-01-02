e14305e3453ad4602816822d3c4c1a5a84fefba3
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-01-02 19:37:25 +0900

    refactoring

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,25 +1,23 @@
 require('dotenv').config()
 const { GoogleSpreadsheet } = require('google-spreadsheet')
 
-// module.exports = async (text) => {
-async function accessSpreadSheet () {
-  try {
-    const document = new GoogleSpreadsheet(process.env.SPREAD_SHEET_ID)
+module.exports = async (text) => {
+  async function accessSpreadSheet () {
+    try {
+      const document = new GoogleSpreadsheet(process.env.SPREAD_SHEET_ID)
 
-    await document.useServiceAccountAuth({
-      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL.replace(/\\n/g, '\n'),
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
-    })
+      await document.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL.replace(/\\n/g, '\n'),
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+      })
 
-    await document.loadInfo()
-    console.log(document.title)
-
-    // const sheet = document.sheetsByIndex[0]
-    // await sheet.addRow({ name: 'Larry Page', email: 'larry@google.com' });
-  } catch (error) {
-      console.log({ error })
+      await document.loadInfo()
+      const sheet = document.sheetsByIndex[0]
+      await sheet.addRow([text])
+    } catch (error) {
+        console.log({ error })
+    }
   }
+  accessSpreadSheet()
 }
-
-accessSpreadSheet()
 
